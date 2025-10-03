deb8a86af23e778cdcc228b5366943b8a31e44da
Chris Rodenas <crodenas@lilly.com>
2025-10-03 13:01:53 -0400

    refactor: improve campaign functions for clarity and consistency

--- src/campaign.py
+++ src/campaign.py
@@ -12,29 +12,30 @@ from models import Campaign
 def list_campaigns() -> List[Campaign]:
     "function"
     with sessionmaker(bind=echo_engine)() as session:
-        return session.query(CampaignModel).all()
+        campaign_models = session.query(CampaignModel).all()
+    return [to_domain(campaign_model) for campaign_model in campaign_models]
 
 
 def create_campaign(campaign: Campaign) -> Campaign:
     "function"
     with sessionmaker(bind=echo_engine)() as session:
         campaign_model = CampaignModel(
-            name=campaign.name, description=campaign.description
+            name=campaign.name,
+            description=campaign.description,
         )
         session.add(campaign_model)
         session.commit()
         session.refresh(campaign_model)
-        return Campaign(
-            id=campaign_model.id,
-            name=campaign_model.name,
-            description=campaign_model.description,
-        )
+        return to_domain(campaign_model)
 
 
-# # infrastructure/mappers.py
-# def to_domain(model: UserModel) -> User:
-#     return User(id=model.id, email=model.email, name=model.name)
+def to_domain(model: CampaignModel) -> Campaign:
+    "function"
+    return Campaign(id=model.id, name=model.name, description=model.description)
 
 
-# def to_model(user: User) -> UserModel:
-#     return UserModel(id=user.id, email=user.email, name=user.name)
+def to_model(campaign: Campaign) -> CampaignModel:
+    "function"
+    return CampaignModel(
+        id=campaign.id, name=campaign.name, description=campaign.description
+    )
